ea70a6c68e6027758b955a390a7f215e9c2696eb
Israel Batista <israelbatiista19@gmail.com>
2024-01-04 13:31:57 -0300

    feat: add VO address

---
+++ src/entity/addres.ts
@@ -0,0 +1,41 @@
+
+export class Address {
+  _street: string;
+  _number: number;
+  _zip: string;
+  _city: string;
+  _state: string;
+ 
+
+  constructor(street: string, number: number, city: string, state: string, zip: string){
+    this._street = street;
+    this._city = city;
+    this._state = state;
+    this._zip = zip;
+    this._number = number;
+    this.validate();
+  }
+
+
+  validate() {
+    if (!this._street) {
+      throw new Error('Street is required');
+    }
+
+    if (!this._city) {
+      throw new Error('City is required');
+    }
+
+    if (!this._state) {
+      throw new Error('State is required');
+    }
+
+    if (!this._zip) {
+      throw new Error('Zip is required');
+    }
+  }
+
+  toString() {
+    return `${this._street} ${this._number}, ${this._city} ${this._state} ${this._zip}`;
+  }
+}
