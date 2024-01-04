5d01ca30aed595167dbc4c082d4529b2474b4cbb
Israel Batista <israelbatiista19@gmail.com>
2024-01-04 15:08:52 -0300

    feat: set private in properties and add validation

--- src/entity/order.ts
+++ src/entity/order.ts
@@ -1,13 +1,42 @@
 import { OrderItem } from "./order_item";
 
 export class Order {
-  _id: string
-  _customerId: string
-  _items: OrderItem[]
+  private _id: string
+  private _customerId: string
+  private _total: number
+  private _items: OrderItem[]
 
   constructor(id: string, customerId: string, items: OrderItem[]) {
     this._id = id;
     this._customerId = customerId;
     this._items = items;
+    this._total = this.total();
+    this.validate();
+  }
+
+  getTotal() {
+    return this._total;
+  }
+
+  total(): number {
+    return this._items.reduce((acc, item) => acc + item.price(), 0);
+  }
+
+  validate() {
+    if (!this._id) {
+      throw new Error('Id is required');
+    }
+
+    if (!this._customerId) {
+      throw new Error('Customer Id is required');
+    }
+
+    if (this._items.length === 0) {
+      throw new Error('Order must have at least one item');
+    }
+
+    if (this._items.some(item => item.quantity <= 0)) {
+      throw new Error('Quantity must be greater than zero');
+    }
   }
 }
