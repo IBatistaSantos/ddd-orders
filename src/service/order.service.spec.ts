b847e984b53b27049c046a1b407fae62c865a894
Israel Batista <israelbatiista19@gmail.com>
2024-01-04 16:03:34 -0300

    test: add test unit for place order

--- src/service/order.service.spec.ts
+++ src/service/order.service.spec.ts
@@ -1,3 +1,4 @@
+import { Customer } from "../entity/customer"
 import { Order } from "../entity/order"
 import { OrderItem } from "../entity/order_item"
 import { OrderService } from "./order.service"
@@ -15,5 +16,21 @@ describe('OrderService', () => {
     const total = OrderService.total([order, order2])
 
     expect(total).toBe(500)
+  })
+
+  it('should place an order', () => {
+    const customer = new Customer('c1', 'Customer 1')
+    const oderItem = new OrderItem('i2', 'Item 2', 200, 'productId', 2)
+
+    const order = OrderService.placeOrder(customer, [oderItem])
+
+    expect(customer.rewardPoints).toBe(200)
+    expect(order.total()).toBe(400)
+  })
+
+  it('should throw error when place an order without items', () => {
+    const customer = new Customer('c1', 'Customer 1')
+
+    expect(() => OrderService.placeOrder(customer, [])).toThrow('Order must have at least one item')
   })
 })
